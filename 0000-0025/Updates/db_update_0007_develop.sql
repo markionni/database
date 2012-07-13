07e081d90694cddb91a411e5699f0e27e3bb8ade
Salja <salja2012@hotmail.de>
2012-07-13 15:47:13 +0200

    [DEV 0007] Some Backports from UDB.
    Signed-off-by: Salja <salja2012@hotmail.de>

--- 0000-0025/Updates/db_update_0007_develop.sql
+++ 0000-0025/Updates/db_update_0007_develop.sql
@@ -22,3 +22,53 @@ INSERT INTO quest_end_scripts VALUES
 (411,23,0,0,0,5666,10,0,'2000000210','0','0','0',0,0,0,0,'Visage Say 3'),
 (411,23,1,2,0,5666,10,0,'0','0','0','0',0,0,0,0,''),
 (411,30,0,0,0,1498,10,0,'2000000211','0','0','0',0,0,0,0,'Bethor Say');
+
+-- NPC Apothecary Antonivich (TBC)
+UPDATE creature_template SET gossip_menu_id=7810 WHERE entry=16588;
+
+DELETE FROM gossip_menu_option WHERE menu_id=7810;
+INSERT INTO gossip_menu_option (menu_id,id,option_icon,option_text,option_id,npc_option_npcflag,action_menu_id,action_poi_id,action_script_id,box_coded,box_money,box_text,cond_1,cond_1_val_1,cond_1_val_2,cond_2,cond_2_val_1,cond_2_val_2,cond_3,cond_3_val_1,cond_3_val_2) VALUES
+(7810,0,3,'I would like to train.',5,16,0,0,0,0,0,'',0,0,0,0,0,0,0,0,0),
+(7810,1,1,'Let me browse your goods.',3,128,0,0,0,0,0,'',0,0,0,0,0,0,0,0,0);
+
+-- NPC Innkeeper Bazil Olof'tazun (TBC)
+UPDATE creature_template SET gossip_menu_id=7288 WHERE entry=18905;
+
+DELETE FROM gossip_menu_option WHERE menu_id=7288;
+INSERT INTO gossip_menu_option (menu_id,id,option_icon,option_text,option_id,npc_option_npcflag,action_menu_id,action_poi_id,action_script_id,box_coded,box_money,box_text,cond_1,cond_1_val_1,cond_1_val_2,cond_2,cond_2_val_1,cond_2_val_2,cond_3,cond_3_val_1,cond_3_val_2) VALUES
+(7288,0,5,'Make this inn your home.',8,65536,0,0,0,0,0,'',0,0,0,0,0,0,0,0,0),
+(7288,1,1,'Let me browse your goods.',3,128,0,0,0,0,0,'',0,0,0,0,0,0,0,0,0);
+
+-- NPC Floyd Pinkus (TBC)
+UPDATE creature_template SET gossip_menu_id=7290 WHERE entry=16602;
+
+DELETE FROM gossip_menu_option WHERE menu_id=7290;
+INSERT INTO gossip_menu_option (menu_id,id,option_icon,option_text,option_id,npc_option_npcflag,action_menu_id,action_poi_id,action_script_id,box_coded,box_money,box_text,cond_1,cond_1_val_1,cond_1_val_2,cond_2,cond_2_val_1,cond_2_val_2,cond_3,cond_3_val_1,cond_3_val_2) VALUES
+(7290,0,5,'A fine offer, Floyd. I wish to make Hellfire Peninsula my home.',8,65536,0,0,0,0,0,'',0,0,0,0,0,0,0,0,0),
+(7290,1,1,'I\'m looking for some specialty goods. Let me browse your wares, Floyd.',3,128,0,0,0,0,0,'',0,0,0,0,0,0,0,0,0);
+
+DELETE FROM gossip_menu WHERE entry=7290;
+INSERT INTO gossip_menu (entry, text_id) VALUES
+(7290, 8634);
+
+-- NPC Magister Aledis, mount and gossip (TBC)
+DELETE FROM creature_template_addon WHERE entry=20159;
+INSERT INTO creature_template_addon (entry,mount,bytes1,b2_0_sheath,b2_1_flags,emote,moveflags,auras) VALUES
+(20159,18696,0,0,0,0,0,'');
+
+UPDATE creature_template SET gossip_menu_id=8081, npcflag=3 WHERE entry=20159;
+
+DELETE FROM gossip_menu_option WHERE menu_id=8081;
+INSERT INTO gossip_menu_option (menu_id,id,option_icon,option_text,option_id,npc_option_npcflag,action_menu_id,action_poi_id,action_script_id,box_coded,box_money,box_text,cond_1,cond_1_val_1,cond_1_val_2,cond_2,cond_2_val_1,cond_2_val_2,cond_3,cond_3_val_1,cond_3_val_2) VALUES
+(8081,0,0,'Magistrix Carinda has sent me to ask you about Arelion\'s secret.  Who was he seeing?',1,1,-1,0,0,0,0,'',9,10286,0,0,0,0,0,0,0);
+
+DELETE FROM gossip_menu WHERE entry=8081;
+INSERT INTO gossip_menu (entry, text_id) VALUES
+(8081,9988);
+
+-- NPC Kagrosh right position (TBC)
+UPDATE creature SET position_x=-3421.365, position_y=2289.388, position_z=33.63479, orientation=3.874631 WHERE guid=86870;
+
+-- NPC Flanis Swiftwing right position and add missing aura (TBC)
+UPDATE creature SET position_x=-3410.419, position_y=2320.867, position_z=37.1792, orientation=4.36332 WHERE guid=86861;
+UPDATE creature_template_addon SET auras=29266 WHERE entry=21727;
