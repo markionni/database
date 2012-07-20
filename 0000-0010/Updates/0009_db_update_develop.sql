f94686a37d83269ee2ce196bdbf864916e70350d
Salja <salja2012@hotmail.de>
2012-07-20 10:29:46 +0200

    [DEV 0009] Some Karazahn Update by Xfurry for SD2 Script Update.
    Signed-off-by: Salja <salja2012@hotmail.de>

--- 0000-0010/Updates/0009_db_update_develop.sql
+++ 0000-0010/Updates/0009_db_update_develop.sql
@@ -17,6 +17,23 @@ INSERT INTO spell_script_target VALUES
 UPDATE creature_template SET unit_flags=unit_flags|33600 WHERE entry=23576;
 
 -- ------------------------------------ --
+--              Karazhan                --
+-- ------------------------------------ --
+
+UPDATE creature_template SET unit_flags=unit_flags&~33554432 WHERE entry=17096;
+
+UPDATE creature_template SET MovementType=0 WHERE entry IN (17007,19872,19873,19874,19875,19876);
+
+DELETE FROM creature_linking_template WHERE entry IN (17007,19872,19873,19874,19875,19876);
+INSERT INTO creature_linking_template VALUES
+(17007,532,15687,3,0),
+(19872,532,15687,3,0),
+(19873,532,15687,3,0),
+(19874,532,15687,3,0),
+(19875,532,15687,3,0),
+(19876,532,15687,3,0);
+
+-- ------------------------------------ --
 --             Scriptdev2               --
 -- ------------------------------------ --
 
